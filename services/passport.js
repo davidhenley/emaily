54959a096034043db9e299f640caad6b8d02fad4
David Henley <henleydr@gmail.com>
2017-08-04 14:40:16 -0500

    Saving user to MongoDB

--- services/passport.js
+++ services/passport.js
@@ -1,13 +1,25 @@
 const passport = require('passport');
+const mongoose = require('mongoose');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+const getOrSaveUser = (accessToken, refreshToken, profile, done) => {
+  User.findOne({ googleId: profile.id }).then(existingUser => {
+    if (existingUser) {
+      done(null, existingUser);
+    } else {
+      new User({ googleId: profile.id }).save()
+        .then(user => done(null, user));
+    }
+  });
+};
 
 passport.use(
   new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
-  }, (accessToken, refreshToken, profile, done) => {
-    console.log(JSON.stringify(profile, null, 2));
-  })
+  }, getOrSaveUser)
 );
