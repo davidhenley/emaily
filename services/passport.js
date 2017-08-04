a72e38fcd9f6d804b1f6b9d09f4a495c8781f01d
David Henley <henleydr@gmail.com>
2017-08-04 15:07:14 -0500

    Serialize User

--- services/passport.js
+++ services/passport.js
@@ -5,11 +5,19 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+  // Sends along the Mongo ID (Not the googleId)
+  // Can't assume everyone will have googleId (with multiple auth)
+  done(null, user.id);
+});
+
 const getOrSaveUser = (accessToken, refreshToken, profile, done) => {
   User.findOne({ googleId: profile.id }).then(existingUser => {
     if (existingUser) {
+      // If there is already a user, find that user and send it along
       done(null, existingUser);
     } else {
+      // Otherwise, create a new user and send that user along
       new User({ googleId: profile.id }).save()
         .then(user => done(null, user));
     }
