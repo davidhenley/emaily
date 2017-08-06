73fab4d6f7adc19ad18e74d4c99024c8589e3ca5
David Henley <henleydr@gmail.com>
2017-08-06 18:41:04 -0500

    Folder structure change

---
+++ server/config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: '221442184602-cnair1riumah5cfhijtj5q0qo06h471d.apps.googleusercontent.com',
+  googleClientSecret: 'nYs0QSt6p_EKYtkaE72Of0se',
+  mongoURI: 'mongodb://david:david@ds161069.mlab.com:61069/my-emaily-dev',
+  sessionKey: 'sjadshdfjsdkfh**&SD&^&^76576sdsad'
+};
