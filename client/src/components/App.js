c00e56f803d43ca0703a97c07aff9a6dca5211aa
David Henley <henleydr@gmail.com>
2017-08-07 14:08:44 -0500

    Header component Boilerplate

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-// Dummy data
-const Header = () => <h2>Header</h2>;
+import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
 const App = (props) => {
   return (
-    <div>
+    <div className="App">
       {/* BrowserRouter can only have one child */}
       <BrowserRouter>
         <div>
