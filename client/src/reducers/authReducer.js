50b267fef76e896a5214e6ad1443f3c45dd4e3ab
David Henley <henleydr@gmail.com>
2017-08-07 16:34:30 -0500

    Auth Reducer FETCH_USER

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,5 +1,12 @@
-export default (state = {}, action) => {
+import {
+  FETCH_USER
+} from '../actions/types';
+
+export default (state = null, action) => {
   switch (action.type) {
+    case FETCH_USER:
+      // returns the user model or false if none found
+      return action.payload || false;
     default:
       return state;
   }
