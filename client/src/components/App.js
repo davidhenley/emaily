2f8058bd0183d74ca05c991e3c854e76ba208376
David Henley <henleydr@gmail.com>
2017-08-07 16:16:48 -0500

    fetchUser action and change to server logout route

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchUser } from '../actions';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Header from './Header';
+
+// Dummy components
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
-const App = (props) => {
-  return (
-    <div className="App">
-      {/* BrowserRouter can only have one child */}
-      <BrowserRouter>
-        <div>
-          {/* Show Header regardless of route */}
-          <Header />
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/surveys" component={Dashboard} />
-          <Route path="/surveys/new" component={SurveyNew} />
-        </div>
-      </BrowserRouter>
-    </div>
-  );
-};
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
 
-export default App;
+  render() {
+    return (
+      <div className="container">
+        {/* BrowserRouter can only have one child */}
+        <BrowserRouter>
+          <div>
+            {/* Show Header regardless of route */}
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = ({ auth }) => ({ auth });
+
+export default connect(mapStateToProps, { fetchUser })(App);
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -9,7 +9,7 @@ module.exports = (app) => {
   // User will have the code, exchange for user profile
   app.get('/auth/google/callback', passport.authenticate('google'));
 
-  app.get('/auth/logout', (req, res) => {
+  app.get('/api/logout', (req, res) => {
     req.logout();
     res.send(req.user);
   });
