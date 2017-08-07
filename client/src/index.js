5dcf53efbb2b992dcc5d438005c200e4c589914c
David Henley <henleydr@gmail.com>
2017-08-07 09:34:59 -0500

    Redux Boilerplate

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+import App from './components/App';
+
+const store = createStore(() => [], {}, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.querySelector('#root'));
