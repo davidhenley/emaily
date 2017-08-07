9c3a2445d5633571d11ee84814eb38c9f0b9b994
David Henley <henleydr@gmail.com>
2017-08-07 14:28:01 -0500

    Header styling

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,9 +3,18 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <div className="Header">
-        Header
-      </div>
+      <nav>
+        <div className="nav-wrapper">
+          <a className="left brand-logo" href="">
+            Emaily
+          </a>
+          <ul className="right">
+            <li>
+              <a href="">Login With Google</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
