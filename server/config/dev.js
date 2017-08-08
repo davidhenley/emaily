6ac69d2fcf7bd4c5856151be4ecc82795c6e15c1
David Henley <henleydr@gmail.com>
2017-08-08 15:06:23 -0500

    Remove serviceworker

--- server/config/dev.js
+++ server/config/dev.js
@@ -2,11 +2,7 @@ module.exports = {
   googleClientID: '221442184602-cnair1riumah5cfhijtj5q0qo06h471d.apps.googleusercontent.com',
   googleClientSecret: 'nYs0QSt6p_EKYtkaE72Of0se',
   mongoURI: 'mongodb://david:david@ds161069.mlab.com:61069/my-emaily-dev',
-<<<<<<< HEAD
   sessionKey: 'sjadshdfjsdkfh**&SD&^&^76576sdsad',
   stripePublishableKey: 'pk_test_SlGgPxoGcP4ryyTfSpBGnjrj',
   stripeSecretKey: 'sk_test_57Xlj9DTTdZUJNvSjogRmRiv'
-=======
-  sessionKey: 'sjadshdfjsdkfh**&SD&^&^76576sdsad'
->>>>>>> 821d453b10b6f83e0910a8d05a64bfbd4f53dc97
 };
