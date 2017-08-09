91f8c1b0ff99b719d84683d80d509fb78a867d43
David Henley <henleydr@gmail.com>
2017-08-09 09:38:34 -0500

    Saving credits to user model

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -10,7 +10,10 @@ router.post('/api/stripe', async (req, res) => {
     source: req.body.id,
     description: "$5 for 5 credits"
   });
-  console.log(charge);
+
+  req.user.credits += 5;
+  const user = await req.user.save();
+  res.send(user);
 });
 
 module.exports = router;
