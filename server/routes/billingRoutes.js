17c4faf3251aa116d2e2e149342da1e229ce7838
David Henley <henleydr@gmail.com>
2017-08-09 09:45:59 -0500

    Making sure user is logged in before accessing /api/stripe

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -4,6 +4,12 @@ const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
 
 router.post('/api/stripe', async (req, res) => {
+  // Make sure user is logged in (Temp fix until middleware)
+  if (!req.user) {
+    return res.status(401).send({ error: 'You must be logged in' });
+  }
+
+  // Create charge on Stripe's API
   let charge = await stripe.charges.create({
     amount: 500,
     currency: "usd",
@@ -11,6 +17,7 @@ router.post('/api/stripe', async (req, res) => {
     description: "$5 for 5 credits"
   });
 
+  // Add 5 credits to user model, save user model, then send new user.
   req.user.credits += 5;
   const user = await req.user.save();
   res.send(user);
