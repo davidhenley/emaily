845986d800d2d072977c16022a9162fb97c7c3bc
David Henley <henleydr@gmail.com>
2017-08-09 09:03:29 -0500

    Finalizing Stripe charge

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const keys = require('../config/keys');
+const stripe = require('stripe')(keys.stripeSecretKey);
 
-router.post('/api/stripe', (req, res) => {
-  console.log(req.body)
+router.post('/api/stripe', async (req, res) => {
+  let charge = await stripe.charges.create({
+    amount: 500,
+    currency: "usd",
+    source: req.body.id,
+    description: "$5 for 5 credits"
+  });
+  console.log(charge);
 });
 
 module.exports = router;
