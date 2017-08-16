311f1c348c0fdf05adba4306684fa7a1bcad2f09
David Henley <henleydr@gmail.com>
2017-08-16 09:16:16 -0500

    Survey Form components

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import SurveyField from './SurveyField';
+
+const FIELDS = [
+  { label: 'Survey Title', name: 'title' },
+  { label: 'Subject Line', name: 'subject' },
+  { label: 'Email Body', name: 'body' },
+  { label: 'Recipient List', name: 'recipients' }
+];
 
 class SurveyForm extends Component {
+  renderFields() {
+    return FIELDS.map(({ name, label }, i) => (
+      <Field type="text" name={name} label={label} component={SurveyField} key={i} />
+    ));
+  }
+
   render() {
     return (
       <div>
-        Survey Form
+        <form onSubmit={this.props.handleSubmit(vals => console.log(vals))}>
+          {this.renderFields()}
+          <button type="submit">Submit</button>
+        </form>
       </div>
     );
   }
